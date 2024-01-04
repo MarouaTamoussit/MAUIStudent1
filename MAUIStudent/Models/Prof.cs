fad6f16f17d677e47d5402898b4505afee3029ec
Lamrisseloubna1 <lamrisse.loubna245@gmail.com>
2024-01-04 19:24:11 +0100

    first commit

---
+++ MAUIStudent/Models/Prof.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUIStudent.Models
+{
+    public class Prof
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public String username { get; set; }
+
+        public String password { get; set; }
+
+
+    }
+}
---
+++ MAUIStudent/Models/students.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+using System.Threading.Tasks;
+
+namespace MAUIStudent.Models
+{
+    public class Students
+    {
+        [PrimaryKey]
+        public String CIN { get; set; }
+        public String nom { get; set; }
+
+        public String prenom { get; set; }
+
+        public String email { get; set; }
+
+        public String tel { get; set; }
+
+        public String Filiere { get; set; }
+
+
+    }
+}
